889a125ee61a65db3b090c65f8e4671b66cab3f3
kazumiviray-hub <viraykazumi@gmail.com>
2026-01-30 08:41:54 +0800

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+
+public class Student {
+    String name;
+    int age;
+    String course;
+
+    // Method to display details
+    void displayInfo() {
+        System.out.println("");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Course: " + course);
+    }
+}
+    
+
